d090c7c3e8231e71e80a68d53c4f1784b2a27b17
Jalia Evans <irohdesign@outlook.com>
2018-02-24 15:41:59 -0500

    fixed formatting issue from typo

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 	
 	<link href="<?php bloginfo('stylesheet_directory');?>/assets/css/bootstrap.min.css" rel="stylesheet">
 	
-	<link href="<?php bloginfo('stylesheet_directory');?>/assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	
 
 	<?php wp_head(); ?>
