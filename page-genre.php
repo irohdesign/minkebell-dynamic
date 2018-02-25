2fc6e1d7fec508c917ad4525eb0f442ffa788474
Jalia Evans <irohdesign@outlook.com>
2018-02-25 10:37:24 -0500

    working on displaying posts by category with better formatting

---
+++ page-genre.php
@@ -0,0 +1,84 @@
+<?php 
+/*
+	Template Name: Home Page		
+*/
+
+get_header(); ?>
+		
+<section class="page-top">
+			<h1>Multi-Post View</h1>
+		</section><!-- page-top end -->
+		
+		<section class="page-wrapper">
+			
+			<section id="multi-wrapper" class="index-inner">
+				
+				<div class="row">
+					<div class="col-sm-6">
+						<a href="">
+							<div class="excerpt-half-img" style="background: url('assets/img/j cole.jpg');
+	background-size: cover;"></div>
+							<div class="excerpt-half-sum">
+								<h3>Here's a sample title</h3>
+								<p>Freakish things keep happening in 2017, most of them awful, but at least one anomaly is for the better. Popular music's most exciting and innovative young artist – the best rapper of his generation, and that's just the start – has somehow become one of its biggest. And Lamar landed there without compromise, after releasing three classic albums in a row.</p>
+							</div>
+						</a>
+					</div>
+					
+					<div class="col-sm-6">
+						<a href="">
+							<div class="excerpt-half-img" style="background: url('assets/img/chancetherapper.jpeg');
+	background-size: cover;"></div>
+							<div class="excerpt-half-sum">
+								<h3>Here's a sample title</h3>
+								<p>Freakish things keep happening in 2017, most of them awful, but at least one anomaly is for the better. Popular music's most exciting and innovative young artist – the best rapper of his generation, and that's just the start – has somehow become one of its biggest. And Lamar landed there without compromise, after releasing three classic albums in a row.</p>
+							</div>
+						</a>
+					</div>
+				</div><!-- row end -->
+				
+				<div class="row">
+					<div class="col-sm-4">
+						<a href="">
+							<div class="excerpt-third-img" style="background: url('assets/img/chancetherapper.jpeg');
+	background-size: cover;">
+								<div class="excerpt-third-title">
+									<h4>Chance the Rapper</h4>
+								</div>
+							</div>
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						<a href="">
+							<div class="excerpt-third-img" style="background: url('assets/img/chancetherapper.jpeg');
+	background-size: cover;">
+								<div class="excerpt-third-title">
+									<h4>Atmosphere</h4>
+								</div>
+							</div>
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						<a href="">
+							<div class="excerpt-third-img" style="background: url('assets/img/chancetherapper.jpeg');
+	background-size: cover;">
+								<div class="excerpt-third-title">
+									<h4>J Cole</h4>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div><!-- row end -->
+				
+				
+			</section><!-- index-inner end -->
+			
+			
+		</section><!-- page-wrapper end -->
+
+
+<?php 
+	get_footer();
+?>
