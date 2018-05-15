84fa1a2d2d91d26cc3e0d9be9806bfa50834f8da
Jalia Evans <irohdesign@outlook.com>
2018-05-15 16:07:01 -0400

    working on grabbing posts

--- page-home.php
+++ page-home.php
@@ -41,7 +41,6 @@ get_header(); ?>
 					
 					<?php
   $args = array(
-        'posts_per_page' => 5,
         'meta_key' => 'meta-checkbox',
         'meta_value' => 'yes'
     );
@@ -119,18 +118,30 @@ wp_reset_postdata(); ?>
 				
 				<section class="new-list">
 					
-					<h2 class="new-list-title">New Albums</h2>
+					<h2 class="new-list-title">New Local Music</h2>
 				
 				<div class="index-inner">	
 					<div class="row">
-						<?php
-						$args = array( 
-							'posts_per_page' => 4,
-							'category'		=> 'rap',
-						);
-						$postslist = get_posts( $args );
-						foreach ($postslist as $post) :  setup_postdata($post); ?> 
-						<div class="col-sm-6 col-md-3">
+						
+						<?php $query = new WP_Query( $args );
+ 
+if ( $query->have_posts() ) { ?>
+ 
+    <section class="<?php echo $category->name; ?> listing">
+
+ 
+        <?php while ( $query->have_posts() ) {
+ 
+            $query->the_post();
+            
+            $args = array(
+					'cat' => 'rap',
+					'post_type' => 'post',
+					'posts_per_page' => '4',
+					'post__not_in' => $do_not_duplicate
+				);
+            ?>
+			<div class="col-sm-6 col-md-3">
 							
 								<a href="<?php the_permalink();?>"><div class="excerpt-thumbnail" style="background: url(<?php the_post_thumbnail_url();?>); background-size: cover;"></div> 
 
@@ -140,9 +151,16 @@ wp_reset_postdata(); ?>
 							<?php echo excerpt(40); ?>
 								
 								 </div><!-- item end -->
-								  <?php endforeach; ?>
-											
-					</div><!-- row end -->
+        <?php } // end while ?>
+ 
+    </section>
+ 
+<?php } // end if
+ 
+// Use reset to restore original query.
+wp_reset_postdata(); ?>
+													
+											</div><!-- row end -->
 					</div><!-- index-innner end -->
 					
 					
