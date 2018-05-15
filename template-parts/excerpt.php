dc6a953fa1db685a20c8cffb654c6742a426b7f7
Jalia Evans <irohdesign@outlook.com>
2018-05-15 16:43:07 -0400

    working on excerpt php, so excerpts appear in a two column layout. getting stuck with excerpts appearing in separate divs

--- template-parts/excerpt.php
+++ template-parts/excerpt.php
@@ -13,48 +13,19 @@ $post_type		= get_post_meta( get_the_ID(), 'post_type', true);
 <section class="page-wrapper">								
 
 	<section id="indiv-post" class="index-inner">
+		<div class="row">
+			<div class="col-sm-6 col-md-4">
+				
+				<a href="<?php the_permalink();?>">
+					<div class="feat-img" style="background-image:url(<?php the_post_thumbnail_url();?>);); background-size: cover;"></div>
+					
+					<h2><?php the_title(); ?></h2>
+					<p><?php the_excerpt(); ?></p>
+					<footer class="entry-footer">
+		<?php audiophile_entry_footer(); ?></footer><!-- .entry-footer -->
 
-		
-		<a href="<?php the_permalink();?>"><div class="feat-img" style="background-image:url(<?php the_post_thumbnail_url();?>);); background-size: cover;"></div>
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?></a>
-		
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h2 class="entry-title">', '</h2>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		
-		<?php
-		endif; ?>
-
-		<?php
-			the_excerpt( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'audiophile' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'audiophile' ),
-				'after'  => '</div>',
-			) );
-		?>
-		
-		<footer class="entry-footer">
-		<?php audiophile_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-		
+			</div><!-- col-sm-6 end -->
+				
 		</section><!-- end of indiv-post section -->
 		
 </section><!-- end of page-wrapper -->
