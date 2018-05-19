53c3bd355ab0a21a1476bbda57b885ddd2ebaac5
Jalia Evans <irohdesign@outlook.com>
2018-05-19 13:40:11 -0700

    adding function to display recent posts when I have the full new-list divs installed

--- functions.php
+++ functions.php
@@ -207,6 +207,39 @@ if( isset( $_POST[ 'meta-checkbox' ] ) ) {
 }
 add_action( 'save_post', 'sm_meta_save' );
 
+
+// Recent Posts Function
+
+function recent_posts($no_posts = 10, $excerpts = true) {
+
+   global $wpdb;
+
+   $request = "SELECT ID, post_title, post_excerpt FROM $wpdb->posts WHERE post_status = 'publish' AND post_type='post' ORDER BY post_date DESC LIMIT $no_posts";
+
+   $posts = $wpdb->get_results($request);
+
+   if($posts) {
+
+               foreach ($posts as $posts) {
+                       $post_title = stripslashes($posts->post_title);
+                       $permalink = get_permalink($posts->ID);
+
+                       $output .= '<li><h2><a href="' . $permalink . '" rel="bookmark" title="Permanent Link: ' . htmlspecialchars($post_title, ENT_COMPAT) . '">' . htmlspecialchars($post_title) . '</a></h2>';
+
+                       if($excerpts) {
+                               $output.= '<br />' . stripslashes($posts->post_excerpt);
+                       }
+
+                       $output .= '</li>';
+               }
+
+       } else {
+               $output .= '<li>No posts found</li>';
+       }
+
+   echo $output;
+}
+
 /**
  * Enqueue scripts and styles.
  */
