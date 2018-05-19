b541de3b38eea9a111bf0c0b3087083fa222aaae
Jalia Evans <irohdesign@outlook.com>
2018-05-19 13:40:26 -0700

    fixing single listing posts

--- page-home.php
+++ page-home.php
@@ -69,11 +69,14 @@ endif;
 			
 		</section><!-- end of index-wrapper -->	
 		
-		
 		<div id="index-listing" class="page-wrapper">
 					<h2 class="new-list-title">Recent Posts</h2>
 			<div class="index-inner">
 				
+			<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+ 
+<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+	
 				<div class="single-post row">
 					<div class="col-sm-5">
 						<?php the_post_thumbnail(); ?>
@@ -82,15 +85,21 @@ endif;
 					<div class="col-sm-7">
 						<div class="top">
 							<h3><?php the_title(); ?></h3>
-							<?php the_excerpt(); ?>
+							<?php echo excerpt(100); ?>
 						</div><!-- top end -->
 						
 						<div class="index-category"><?php the_category(); ?></div>
 					</div>
 				</div>
 				
+				
+				<?php 
+					endwhile;
+						wp_reset_postdata();		
+				?>
 			</div><!-- index-inner end -->
 		</div><!-- index-listing end -->
+		
 		
 		
 		
