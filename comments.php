bd93c32d75298076baf9138f582fcfe70eba1e0c
Jalia Evans <irohdesign@outlook.com>
2018-05-25 17:10:46 -0400

    set up comments section so it's dynamic while still fitting in with the design of the website

--- comments.php
+++ comments.php
@@ -19,68 +19,15 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-
-<section id="comments" class="index-inner">
-				<div class="comment-wrapper">
-					
-					<div class="indiv-comment">
-						<div class="comment-body">
-						<p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the.</p>
-						</div><!-- comment-body end -->
-						
-						<div class="comment-info">
-							<h4>Commenter</h4><h4>Comment Date</h4>
-						</div><!-- comment-info end -->
-					</div><!-- indiv-comment end -->
-					
-					<div class="indiv-comment">
-						<div class="comment-body">
-						<p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the.</p>
-						</div><!-- comment-body end -->
-						
-						<div class="comment-info">
-							<h4>Commenter</h4><h4>Comment Date</h4>
-						</div><!-- comment-info end -->
-					</div><!-- indiv-comment end -->
-					
-					
-					<form>
-						<input type="text" placeholder="Your name">
-						<input type="email" placeholder="Your email">
-						
-						<textarea placeholder="Your comment"></textarea>
-						
-						<input type="submit" value="Submit">
-					</form>
-				</div><!-- comment-wrapper end -->
-			</section><!-- comments end -->
-			
-		</section><!-- page-wrapper end -->
-		
-<div id="comments" class="comments-area">
+<div class="page-wrapper">		
+<div class="comment-wrapper"><div id="comments" class="comments-area">
 
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-			$comment_count = get_comments_number();
-			if ( '1' === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'audiophile' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'audiophile' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+
+	<h1>Comments</h1>
+
 		<?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
@@ -106,3 +53,5 @@ if ( post_password_required() ) {
 	?>
 
 </div><!-- #comments -->
+</div>
+</div>
