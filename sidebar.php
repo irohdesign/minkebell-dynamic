6a5d0d6aa589847313c1e6719262807a1897e5a3
Jalia Evans <irohdesign@outlook.com>
2018-05-25 16:34:33 -0400

    showcasing other posts with images to attract attention. also added a searchbar so users can sift through large amounts of posts easily

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,31 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+<h1>More Posts</h1>
+
+<div class="row">
+	
+<?php query_posts('showposts=4'); ?>
+<?php $posts = get_posts('numberposts=5&offset=0'); foreach ($posts as $post) : start_wp(); ?>
+<?php static $count1 = 0; if ($count1 == "4") { break; } else { ?>
+
+<div class="col-sm-4 col-md-3">
+
+<div class="sq-prev" style="background: url(<?php the_post_thumbnail_url(); ?>); background-size: cover;"></div>
+<a href=<?php the_permalink(); ?>><h3><?php the_title(); ?></h3></a>
+<h4><?php the_category(); ?></h4>
+</div>
+
+<?php $count1++; } ?>
+<?php endforeach; ?>
+
+</div>
+
+<div class="search-box">
+	<h3>Looking for something specific?</h3>
+	<?php get_search_form(); ?>
+</div>
+
 </aside><!-- #secondary -->
+
