9228c7eb1e344546ca5337ba0fd1102bc05f2c32
Jalia Evans <irohdesign@outlook.com>
2018-05-28 00:24:22 -0400

    fixed up 404 page so the errors will appear within site styles

--- 404.php
+++ 404.php
@@ -9,47 +9,49 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<section class="page-top">
+<h1 class="page-title"><?php esc_html_e( 'Oh damn.', 'audiophile' ); ?></h1>
+</section>
+
+<section class="page-top">
+<h3>It looks like you found a weakness in my site.</h3>
+<h4>I'll be sure to patch this <strong>ASAP</strong>, but feel free to read these other posts in the meantime.</h4>
+</section>
+
+	<div id="primary" class="content-area page-wrapper">
+		<main id="main" class="site-main index-inner">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'audiophile' ); ?></h1>
-				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'audiophile' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'audiophile' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'audiophile' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				
+			<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+ 
+<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+	
+				<div class="single-post row"><a href="<?php the_permalink();?>">
+					<div class="col-sm-5">
+						<?php the_post_thumbnail(); ?></a>
+					</div>
+					
+					<div class="col-sm-7">
+						<div class="top">
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<?php echo excerpt(80); ?>
+						</div><!-- top end -->
+						
+						<div class="index-category">Posted in:<?php the_category(); ?></div>
+					</div>
+				</div>
+				
+				
+				<?php 
+					endwhile;
+						wp_reset_postdata();		
+				?>
+			</div><!-- index-inner end -->
+		</div><!-- index-listing end -->
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
