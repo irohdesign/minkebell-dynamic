9b9f589ba7a85cdc2cf3ce6387005d5eaae7aa40
Jalia Evans <irohdesign@outlook.com>
2018-05-30 00:52:54 -0400

    added extra margin for more breathability in the design and cleaned up the formatting

--- single.php
+++ single.php
@@ -1,16 +1,5 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package audiophile
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
 		<?php
 		while ( have_posts() ) : the_post();
